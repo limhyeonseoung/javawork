e615cbb5c1f7b9ed2b204fe48831aefda9b3147a
limhyeonseoung <coldfela99@naver.com>
2024-05-07 18:35:43 +0900

    01finall

--- day01/src/_01_print/T02_printf.java
+++ day01/src/_01_print/T02_printf.java
@@ -24,8 +24,11 @@ System.out.printf("%c\n", 'm');
 System.out.printf("%b\n","abcedf");
 //boolean타입 출력
 System.out.printf("%b/n",true);
-System.out.("%b\n",7>3);
+System.out.printf("%b\n", 7>3);
 // 현재는 2024년 5월이고 ,우리나라의 평균키는 165.7입니다
+System.out.printf("현재는 %d년%d월이고 , 우리나라의 평균키는 %f입니다", 2024,5,165.7);
+//이 과정의 정원은 25명이고, 현재는 25명 수강중 입니다. 100%를 달성했습니다
+System.out.printf("이과정의 정원은 %d명이고,현재는%d명 수강중입니다.%d%%를 달성했습니다",25,25,100);
 	}
 
 }
---
+++ day01/src/_03_Scanner/T02_Ex01.java
@@ -0,0 +1,29 @@
+package _03_Scanner;
+
+import java.util.Scanner;
+
+public class T02_Ex01 {
+
+	public static void main(String[] args) {
+		
+	// 국어,컴퓨터,수학 점수를 입력받아 저장하고
+    // 총점과 평균을 출력하세요
+		Scanner scan=new Scanner(System.in);
+		System.out.print("국어점수를 입력하세요 : ");
+		double grade = scan.nextDoublue();
+		System.out.println("점수" + grade);
+		
+		
+	
+		
+		
+	}
+}
+
+	
+		
+	
+
+		
+		
+	
