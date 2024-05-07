78a6fea54516bb86a52ad3ba682b8f48166fe1a9
limhyeonseoung <coldfela99@naver.com>
2024-05-07 14:34:31 +0900

    the day01

---
+++ day01/src/day01/T01_print.java
@@ -0,0 +1,46 @@
+package day01;
+
+public class T01_print {
+
+	public static void main(String[] args) {
+		//한줄주석
+		/*ㄴㅇㄴㅇㅇ
+		 * ㄴㄴㄴㅇㅇ
+		 ㄴㅇㅁㄴㅇㅁㄴㄴㄴㅇ
+		 */
+//		출력문 : println_-> 출력후 줄바꾸기
+//		문자열을출력때는 반드시 쌍다음표("")를 넣어준다
+		System.out.println("12.29");
+ System.out.println("더조은 컴퓨터 학원");
+ System.out.println("배고파요");
+System.out.println("임@현!승");
+System.out.println(6/6*2);
+System.out.println(5+12+23+"ㄴ나나나나");
+// + 기호는 숫자였을 때는 연산을 하고
+//
+System.out.println("강의실:          "     +           "                                  801호");
+System.out.println("연산결과 : "  +  +7+0+5);	
+System.out.println("ㅇㄴ사" + 2+2+3);
+System.out.println("내내내내내내         :       "+3*3);
+
+
+System.out.print("줄바꿈안해");
+System.out.print("진짜안됨");
+System.out.println("sssss");
+System.out.println("임현승 임현승,");
+
+
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
